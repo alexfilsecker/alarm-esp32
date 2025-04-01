4c3e1f67518cfab4958499ed56e5b40d38351428
Alex <alexfilsecker@gmail.com>
2025-04-01 01:42:49 -0300

    buzzer debug codes

--- include/buzzer.h
+++ include/buzzer.h
@@ -4,12 +4,16 @@ class Buzzer
 {
 private:
     const int pin;
+    const bool verbose = false; // Set to true for debugging
 
 public:
     Buzzer(int pin);
+    Buzzer(int pin, int verbose);
 
     void setup();
 
     void beep();
     void beepnt();
+
+    void loopTest();
 };
--- src/buzzer.cpp
+++ src/buzzer.cpp
@@ -7,17 +7,43 @@ Buzzer::Buzzer(int pin)
 {
 }
 
+Buzzer::Buzzer(int pin, int verbose)
+    : pin(pin), verbose(verbose)
+{
+}
+
 void Buzzer::setup()
 {
     pinMode(pin, OUTPUT);
+    if (verbose)
+    {
+        Serial.print("Buzzer pin set to: ");
+        Serial.println(pin);
+    }
 }
 
 void Buzzer::beep()
 {
     digitalWrite(pin, HIGH);
+    if (verbose)
+    {
+        Serial.println("Buzzer beeped!");
+    }
 }
 
 void Buzzer::beepnt()
 {
     digitalWrite(pin, LOW);
+    if (verbose)
+    {
+        Serial.println("Buzzer beepnt!");
+    }
+}
+
+void Buzzer::loopTest()
+{
+    beep();
+    delay(1000);
+    beepnt();
+    delay(1000);
 }
